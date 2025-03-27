68c1ac2f4d4efacd01ba37703835f43bd5f29cae
nathan choi <nath49ers@gmail.com>
2025-03-27 00:59:57 -0700

    hello world

--- Take2Backend/index.ts
+++ Take2Backend/index.ts
@@ -1,1 +1,14 @@
-console.log("hello, world imager!")
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+
+const PORT = 9999;
+const app = express();
+const httpServer = createServer(app);
+
+app.set(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+httpServer.listen(PORT, () => {
+  console.log("SERVER STARTED");
+});
