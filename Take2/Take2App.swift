f195bd3bb95d156bdda1c0ac5d28716d1d5fb617
nathan choi <nath49ers@gmail.com>
2025-03-27 00:30:28 -0700

    Initial Commit

---
+++ Take2/Take2App.swift
@@ -0,0 +1,17 @@
+//
+//  Take2App.swift
+//  Take2
+//
+//  Created by Nathan Choi on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Take2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
