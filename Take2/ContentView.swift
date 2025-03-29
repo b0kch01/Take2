bc5fcc93e6e8c8ab6c1da0c029401017e43ded6e
nathan choi <nath49ers@gmail.com>
2025-03-29 00:47:19 -0700

    cool client base

--- Take2/ContentView.swift
+++ Take2/ContentView.swift
@@ -10,31 +10,40 @@ import PhotosUI
 
 struct ContentView: View {
 
-    @State var imageSelection: PhotosPickerItem? = nil
-    @State var image: Image?
+    @Namespace private var homescreen
+    @Namespace private var buttonBg
+    @Namespace private var buttonText
+
+    @State private var imageSelection: PhotosPickerItem? = nil
+    @State private var image: Image?
 
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
 
-            Text("Image Lab")
-                .font(.title)
-                .fontDesign(.serif)
+            HStack {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Image Lab")
+                        .font(.title)
+                        .fontDesign(.serif)
 
-            HStack(spacing: 5) {
-                Image(systemName: "ticket.fill")
-                    .foregroundStyle(.yellow)
+                    HStack(spacing: 5) {
+                        Image(systemName: "ticket.fill")
+                            .foregroundStyle(.yellow)
 
-                Text("10 Credits")
-                    .font(.caption)
-                    .fontWeight(.semibold)
+                        Text("10 Credits")
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                    }
+                }
+
+                Image(systemName: "")
             }
 
             ZStack {
-                Spacer()
+
+                Color.clear
 
                 if let image {
-                    Color(.secondarySystemBackground)
-
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -52,34 +61,73 @@ struct ContentView: View {
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(style: StrokeStyle(lineWidth: 0.5, dash: [4]))
                 )
-                .animation(.spring(), value: image)
 
             Spacer()
 
-            PhotosPicker(selection: $imageSelection,
-                         matching: .images,
-                         photoLibrary: .shared())
-            {
-                LazyVStack {
-                    Text("Select Photo  \(Image(systemName: "photo"))")
-                        .foregroundStyle(Color(.systemBackground))
-                        .fontWeight(.medium)
-                        .padding(.vertical, 15)
+            HStack(spacing: 15) {
+
+                if image != nil {
+                    Button { withAnimation { image = nil } } label: {
+                        Text("Discard")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.medium)
+                            .padding(.vertical, 15)
+                            .padding(.horizontal, 25)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(.secondarySystemBackground))
+                            )
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.primary)
-                )
+
+                if image == nil {
+                    PhotosPicker(selection: $imageSelection,
+                                 matching: .images,
+                                 photoLibrary: .shared())
+                    {
+                        LazyVStack {
+                            Text("Select Photo")
+                                .foregroundStyle(Color(.systemBackground))
+                                .fontWeight(.medium)
+                                .padding(.vertical, 15)
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.primary)
+                        )
+                        .matchedGeometryEffect(id: buttonBg, in: homescreen)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .transition(.offset())
+                } else {
+                    Button {} label: {
+                        LazyVStack {
+                            Text("\(Image(systemName: "wand.and.stars"))  Generate Results")
+                                .foregroundStyle(Color(.systemBackground))
+                                .fontWeight(.medium)
+                                .padding(.vertical, 15)
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.primary)
+                        )
+                        .matchedGeometryEffect(id: buttonBg, in: homescreen)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .transition(.offset())
+                }
             }
-            .buttonStyle(PlainButtonStyle())
         }
         .padding(15)
         .onChange(of: imageSelection) { oldValue, newValue in
-            Task {
+            Task(priority: .userInitiated) {
                 if let loaded = try? await imageSelection?.loadTransferable(
                     type: Image.self
                 ) {
-                    image = loaded
+                    withAnimation(.spring()) {
+                        image = loaded
+                    }
                 } else {
                     print("Failed to load image.")
                 }
